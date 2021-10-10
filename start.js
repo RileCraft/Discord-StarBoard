3d86b5043d9a7c51c5b1a2c84ea98551930003ff
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-10-10 21:57:22 +0530

    update start.js

--- start.js
+++ start.js
@@ -1,2 +1,4 @@
 const { exec } = require("child_process")
-exec(`git pull ${process.env.url} && node bot.js`)
+exec(`git pull ${process.env.url} && node bot.js`, (error, stdout) => {
+	if (stdout) console.log("done")
+	})
