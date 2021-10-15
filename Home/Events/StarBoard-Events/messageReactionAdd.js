c0d7b6100633ac1ebba11cc0e325d6f720b1d5e8
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-10-15 12:00:09 +0530

    update messageReactionAdd.js

--- Home/Events/StarBoard-Events/messageReactionAdd.js
+++ Home/Events/StarBoard-Events/messageReactionAdd.js
@@ -1,11 +1,12 @@
 module.exports = {
     name: 'messageReactionAdd',
+    clientPermissions: ["MANAGE_MESSAGES"],
     run: async (reaction, user, client, Discord) => {
         const name = reaction.emoji.name
         if (name != "⭐") return;
         const message = reaction.message
         const author = message.author
-        const size = reaction.count 
+        const size = reaction.count
         const reqSize = await db.get(message.guild.id + ".starboard.requiredStars")
         const sbchannel = message.guild.channels.cache.get(await db.get(message.guild.id + ".starboard.channelID"))
         const allowBot = await db.get(message.guild.id + ".starboard.allowBots") ?? true
@@ -18,20 +19,20 @@ module.exports = {
         if (!allowBot && user.bot) return;
         if (data) {
             if (size < reqSize) return;
-                    let msg = data.message.content
-                    const ii = await db.get(`${message.guild.id}.${data.message.id}`)
-                    const i = await sbchannel.messages.fetch(data.starmsgID)
-                    if (data.message.content === "") msg = "Visit Message"
-                    const embed = new Discord.MessageEmbed()
-                        .setColor("RANDOM")
-                        .setTimestamp(data.message.createdTimestamp)
-                        .setDescription(`[${msg}](https://discord.com/channels/${data.guildID}/${message.channel.id}/${data.message.id})`)
-                        .setAuthor(data.user.tag, data.user.avatar)
-                        .setImage(data.message.attachment)
-                    i.edit({
-                        embeds: [embed],
-                        content: `<#${data.channelID}> | ⭐ **${size}**`
-                    })
+            let msg = data.message.content
+            const ii = await db.get(`${message.guild.id}.${data.message.id}`)
+            const i = await sbchannel.messages.fetch(data.starmsgID)
+            if (data.message.content === "") msg = "Visit Message"
+            const embed = new Discord.MessageEmbed()
+                .setColor("RANDOM")
+                .setTimestamp(data.message.createdTimestamp)
+                .setDescription(`[${msg}](https://discord.com/channels/${data.guildID}/${data.channelID}/${data.message.id})`)
+                .setAuthor(data.user.tag, data.user.avatar)
+                .setImage(data.message.attachment)
+            i.edit({
+                embeds: [embed],
+                content: `<#${data.channelID}> | ⭐ **${size}**`
+            })
         } else {
             if (size < reqSize) return;
             const embed = new Discord.MessageEmbed()
@@ -46,7 +47,7 @@ module.exports = {
                 embeds: [embed],
                 content: `<#${message.channel.id}> | ⭐ **${size}**`
             })
-            
+
             await db.set(`${message.guild.id}.${message.id}`, {
                 "channelID": message.channel.id,
                 "starmsgID": starb.id,
@@ -66,8 +67,8 @@ module.exports = {
                 },
                 "guildID": message.guild.id
             })
-            
+
         }
-        
+
     }
 }
