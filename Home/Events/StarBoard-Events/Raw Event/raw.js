7c10516abddaca505c46c568ca97c761c8a88f92
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-10-15 12:00:44 +0530

    update raw.js

--- Home/Events/StarBoard-Events/Raw Event/raw.js
+++ Home/Events/StarBoard-Events/Raw Event/raw.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: "raw",
     clientPermissions: ["MANAGE_MESSAGES"],
-    run: async(packet) => {
-        if (!["MESSAGE_REACTION_ADD","MESSAGE_REACTION_REMOVE"].some(x => x.includes(packet.t))) return;
+    run: async (packet) => {
+        if (!["MESSAGE_REACTION_ADD", "MESSAGE_REACTION_REMOVE"].some(x => x.includes(packet.t))) return;
         const { client } = require(HOME + "/bot")
         const Discord = require("discord.js")
         if (client.channels.cache.get(packet.d.channel_id).messages.cache.get(packet.d.message_id)) return;
@@ -13,9 +13,8 @@ module.exports = {
         const emoji = packet.d.emoji.name
         if (packet.t == 'MESSAGE_REACTION_ADD') {
             client.emit('onStarAdd', message, emoji, user, client, Discord)
-                    }
-                    else if (packet.t == 'MESSAGE_REACTION_REMOVE') {
-                        client.emit('onStarRemove', message, emoji, user, client, Discord)
-                                }
+        } else if (packet.t == 'MESSAGE_REACTION_REMOVE') {
+            client.emit('onStarRemove', message, emoji, user, client, Discord)
+        }
     }
 }
